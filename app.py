58ed959c41c33fcb9654725cfa622bcaecf1f45d
devin stevenson <emberdesign@gmail.com>
2016-06-12 20:02:16 -0700

    initial

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import (Flask, session, render_template, g,
+                   redirect, url_for, request, flash)
+from functools import wraps
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = 'blahyahflanders49fsld0rD?><?Sfo923ur029'
+app.database = 'sample.db'
+
+# login decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/')
+@login_required
+def home():
+    g.db = connect_db()
+    cur = g.db.execute("select * from posts")
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html', posts=posts)
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        user = request.form.get('username')
+        pw = request.form.get('password')
+        if user != 'admin' or pw != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('welcome'))
+
+def connect_db():
+    return sqlite3.connect(app.database)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
