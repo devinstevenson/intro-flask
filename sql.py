b82fa4a6091b56894b2d40cd5330b0051a3bb0d9
devin stevenson <emberdesign@gmail.com>
2016-06-13 12:32:53 -0700

    add req

--- sql.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-with sqlite3.connect('sample.db') as connection:
-    c = connection.cursor()
-    c.execute("""CREATE TABLE posts(title TEXT, description TEXT)""")
-    c.execute('INSERT INTO posts VALUES ("Good", "I\'m good.")')
-    c.execute('INSERT INTO posts VALUES ("Well", "I\'m well.")')
