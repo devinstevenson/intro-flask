c8b6b506c536264c1eac7220ab8b62127161ac54
devin stevenson <emberdesign@gmail.com>
2016-06-13 00:32:00 -0700

    update config/req

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = u'DnfhKSYPnuKYK/Oqu56D6piX9bXan4XG5m35Mm9OeuM47sp+/rx3ALkNsnaxkVg2GXAQU1nQu/AjwwMTjRL+pQg=='
+    # SECRET_KEY = u'DnfhKSY3ALkNsnaxkVg2GXAQU1nQu/AjwwMTjRL+pQg=='
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
