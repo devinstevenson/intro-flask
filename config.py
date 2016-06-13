e0dbbfc6a775d82f513aadbd03259ff8ee3fa993
devin stevenson <emberdesign@gmail.com>
2016-06-13 10:20:13 -0700

    added migrations

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     # SECRET_KEY = u'DnfhKSY3ALkNsnaxkVg2GXAQU1nQu/AjwwMTjRL+pQg=='
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 from app import db
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 class BlogPost(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
 
     def __init__(self, title, description):
         self.title = title
@@ -13,3 +15,20 @@ class BlogPost(db.Model):
 
     def __repr__(self):
         return '<title {}'.format(self.title)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return "<name {}".format(self.name)
